f44ee71c24dd8ddb81ce6a3b50cdeb91477d6a8c
valentin lemaire <valentinlmr28@gmail.com>
2020-05-13 08:52:05 +0200

    pull

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,29 @@ class PagesController < ApplicationController
 
     def dashboard
         @user = current_user
-        @review = Review.new
+        @allAvailabilities = @user.availability
+        @arrayof5day = week(Date.today)
+        @availabilitiesOfTheWeek = userAvailabilities(@allAvailabilities)
     end 
+
+
+
+    def userAvailabilities(allAvailabilities)
+        datesArray = []
+        allAvailabilities.each do |date|  
+            if date.date > Date.today 
+                 datesArray << date.date
+            end
+        end 
+        return datesArray.sort.first(5)    
+    end
+
+    def week(today)
+        array = []
+        array.push(Date.today)
+        until array.length == 5 
+            array.push(array.last + 1)
+        end
+        return array
+    end
 end
