2f6fb6f8cedb77091fd2da7b3bdae5df7374997c
valentin lemaire <valentinlmr28@gmail.com>
2020-05-14 09:00:28 +0200

    changeType into string for availability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_092421) do
+ActiveRecord::Schema.define(version: 2020_05_14_065656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(version: 2020_05_10_092421) do
 
   create_table "availabilities", force: :cascade do |t|
     t.date "date"
-    t.time "start_time"
-    t.time "end_time"
+    t.string "start_time"
+    t.string "end_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
